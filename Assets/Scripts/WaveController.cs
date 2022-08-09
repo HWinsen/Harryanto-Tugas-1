98cb191be4ecb9b3444da7861faa4d870aa63574
DESKTOP-VQ9J7KA\a-6 <DESKTOP-VQ9J7KA\a-6>
2022-08-09 17:02:11 +0800

    Wave

--- Assets/Scripts/WaveController.cs
+++ Assets/Scripts/WaveController.cs
@@ -1,17 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class WaveController : MonoBehaviour
 {
     private float _waveCountdown;
     private int _waveCount;
+    [SerializeField] private TMP_Text _waveTMP;
 
     // Start is called before the first frame update
     void Start()
     {
         _waveCountdown = 10f;
         _waveCount = 1;
+        _waveTMP.SetText("Wave: " + _waveCount.ToString());
     }
 
     // Update is called once per frame
@@ -20,7 +23,9 @@ public class WaveController : MonoBehaviour
         _waveCountdown -= Time.deltaTime;
         if (_waveCountdown <= 0)
         {
+            Debug.Log(_waveCountdown);
             _waveCount++;
+            _waveTMP.SetText("Wave: " + _waveCount.ToString());
             _waveCountdown = 10f;
         }
     }
